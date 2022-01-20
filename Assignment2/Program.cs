f0773de2afc5ca11c88814758b14f96c636aac03
afteraryan <saxenaa039@gmail.com>
2022-01-20 15:44:44 +0530

    ColorAssignment

---
+++ Assignment2/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+class StoryColor
+{
+    static void Main(string[] args)
+    {
+        Console.ForegroundColor = ConsoleColor.Red;
+        Console.WriteLine("Wellerrman");
+        Console.ForegroundColor = ConsoleColor.Blue;
+        Console.WriteLine("There once was a ship that put to sea \nThe name of the ship was the Billy of Tea");
+        Console.ResetColor();
+    }
+}
