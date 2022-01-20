23819addb54c96084bfae41bf10a9aae19d17ecb
afteraryan <saxenaa039@gmail.com>
2022-01-20 15:03:22 +0530

    Unityc#

---
+++ C#/HelloWorld.cs
@@ -0,0 +1,12 @@
+using System; 
+namespace HelloWorldPoject
+{
+    class Program
+    {
+        static void main(string[] args)
+        {
+            console.WriteLine("Hello World!");
+            console.ReadKey();
+        }
+    }
+}
